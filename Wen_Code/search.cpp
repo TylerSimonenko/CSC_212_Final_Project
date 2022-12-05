c134c3cebc806d0007b43a47f68c45ad3859a0ad
cwen078 <78177731+cwen078@users.noreply.github.com>
2022-12-05 16:03:52 -0500

    Update search.cpp

--- Wen_Code/search.cpp
+++ Wen_Code/search.cpp
@@ -3,3 +3,48 @@
 #include <string>
 
 using namespace std;
+void search(char pat[], char txt[], int q)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+    int i, j;
+    int p = 0; 
+    int t = 0; 
+    int h = 1;
+ 
+    for (i = 0; i < M-1; i++)
+        h = (h*d)%q;
+ 
+    for (i = 0; i < M; i++)
+    {
+        p = (d*p + pat[i])%q;
+        t = (d*t + txt[i])%q;
+    }
+ 
+    // Slide the pattern over text one by one
+    for (i = 0; i <= N - M; i++)
+    {
+        if ( p == t )
+        {
+            for (j = 0; j < M; j++)
+            {
+                if (txt[i+j] != pat[j])
+                    break;
+            }
+ 
+            // if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
+            if (j == M)
+                //printf("Pattern found at index %d \n", i);
+                cou++;
+        }
+ 
+      
+        if ( i < N-M )
+        {
+            t = (d*(t - txt[i]*h) + txt[i+M])%q;
+ 
+            if (t < 0)
+            t = (t + q);
+        }
+    }
+}
