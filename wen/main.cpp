254be1c0e397e339521b3f117b1e9196bc8d554e
cwen078 <78177731+cwen078@users.noreply.github.com>
2022-12-04 23:40:36 -0500

    Create main.cpp

---
+++ wen/main.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+     int main()
+     { using namespace std
+      std::ifstream file;
+    file.open("testfile.txt");
+    
+    file.getline(txt),
+    clock_t begin = clock();
+    search(txt,pat);
+    clock_t end = clock();
+    double passed_seconds = double(end - begin)*1000 / CLOCKS_PER_SEC;
+    
+    cout<<"Number of matches of \""<<pat<<"\" is "<<cou<<endl<<"Time taken:"<<elapsed_secs<<endl;
+    
+    
+  clock_t t;
+  
+  t = clock();
+  
+  printf ("processing...\n");
+
+  t = clock() - t;
+  
+  printf ("It took %f seconds.\n",t,((float)t)/CLOCKS_PER_SEC);
+  return 0;
+}
