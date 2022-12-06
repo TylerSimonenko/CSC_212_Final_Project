bc6997b03a56ecd2039e558e72458e9a8fc62d7c
marshalllele <98423695+marshalllele@users.noreply.github.com>
2022-12-05 23:49:05 -0500

    Update final.h

--- Marshall_Code/Final Copies/final.h
+++ Marshall_Code/Final Copies/final.h
@@ -1,1 +1,28 @@
+// Including Libs
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <chrono>
 
+#ifndef TYLER_H
+#define TYLER_H
+
+class proj {
+ 
+    public:
+        void readFile(std::vector<char>& text, std::string filename, std::vector<int>& index);
+        std::vector<int> rabinKarp(std::vector<char>& text, std::vector<char>& search, bool& found, std::vector<int>& rowOcur, std::vector<int> index);
+        void boyerMoore(std::vector<char>& text, std::vector<char>& search);
+        void naive(std::vector<char> text, std::vector<char> search);
+        void writeFile(std::vector<char>& text, std::string outputName, std::vector<int>& index, int phraseDif, std::vector<int> rowOcur);
+        std::vector<char> getPhrase();
+        void replace(std::vector<char>& text, std::vector<int> index, std::vector<char> search, bool found, int&phraseDif);
+
+
+
+};
+
+#endif
